2a78671f5e14b252d812f73fc68e2570e947ebab
Bernard Banta <banta.bernard@gmail.com>
2012-11-13 06:09:23 +0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NairubySite::Application.config.secret_token = '935850eb461c016c28d5a87d76468d1b7c2ddc6d31b3ce382735562c0ec4ccf2dc3ea36ec580d52f41db7cfee15af699997c5e772defeb3b500e9f4f61f27317'
