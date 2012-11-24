44c99f4621091558033cffbd2c65bffc8fe8a7d6
Bernard Banta <banta.bernard@gmail.com>
2012-11-24 17:04:14 +0300

    Added home/calendar in the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ NairubySite::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
   end
+  match 'home/calendar' => 'home#calendar', :via => :get
   root :to => "home#index"
   devise_for :users
   resources :users
