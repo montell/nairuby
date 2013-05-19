befd01a962691e70a9958e5c5e90f3f600d1c273
Bernard Banta <banta.bernard@gmail.com>
2013-05-19 11:50:38 +0300

    Added mebers page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,18 @@ class HomeController < ApplicationController
   
   def calendar
   end
+
+  def members
+  	@arr_users = []
+  	@users = User.all
+  	User.all.each do |user|
+  		@arr_users << user.id
+  	end
+
+  	@members = @arr_users.count
+
+
+  	@rows = @members/4
+  	@rem_rows = @members%4
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ NairubySite::Application.routes.draw do
     root :to => 'home#index'
   end
   match 'home/calendar' => 'home#calendar', :via => :get
+  match 'home/members' => 'home#members', :via => :get
   root :to => "home#index"
   devise_for :users
   resources :users
