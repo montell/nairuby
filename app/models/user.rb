aaab908e3d2e1cbdce60a232d80d474aa7f301f4
Bernard Banta <banta.bernard@gmail.com>
2013-05-19 23:05:06 +0300

    Name is required in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # Validations
+  validates :name, :presence => true
+
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me,
