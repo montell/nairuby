f6b2aaf7ca9cde983b058e81e78e0183ca73d5a8
Bernard Banta <banta.bernard@gmail.com>
2013-08-01 14:04:05 +0300

    Added a 'hi user' to the notification user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,20 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  layout 'emails'
+  default from: "nairubygroup@gmail.com", reply_to: "nairubygroup@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.notification.subject
   #
-  def notification
-    @greeting = "Hi"
+  def notification(user, subject, message)
+    @message = message
+    @user = user
 
-    mail to: "to@example.org"
+    if !user.email.nil?
+      mail(:to => @user.email, :subject => subject) do |format|
+        format.html
+      end
+    end
   end
 end
