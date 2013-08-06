b9f9fe7f1a9928955f35533978183c14c295f91d
Bernard Banta <banta.bernard@gmail.com>
2013-08-06 11:18:50 +0300

    Generate delayed_job:active_record and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130801090901) do
+ActiveRecord::Schema.define(:version => 20130806081759) do
 
   create_table "attachments", :force => true do |t|
     t.datetime "created_at",              :null => false
